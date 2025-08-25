27cb8400badd335b4ef7a1b79b1f991640b3df5b
Sanghita Seal <sealsanghita2005@gmail.com>
2025-08-25 09:57:57 +0530

    Add bcrypt for password hashing and comparison in user model

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,5 +1,5 @@
 import mongoose, {Schema} from "mongoose";
-
+import bcrypt from "bcrypt"
 const userSchema = new Schema({
     avatar:{
         type:{
@@ -58,4 +58,15 @@ const userSchema = new Schema({
     timestamps:true
 })
 
-export const User = mongoose.model("User", userSchema);
+//pre hook written for hashing the password
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password"))  return next()
+    this.password = await bcrypt.hash(this.password,10)
+    next()
+})
+
+//method of comparing the hashed password with the actual password
+userSchema.methods.isPasswordCorrect= async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+//export const User = mongoose.model("User", userSchema);
